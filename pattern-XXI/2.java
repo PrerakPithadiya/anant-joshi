f11335ad0c81c28b71dd7cf2319e328e27b5fbb7
Prerak Pithadiya <prerakpithadiya@gmail.com>
2024-03-31 20:23:01 +0530

    Update 2.java

--- pattern-XXI/2.java
+++ pattern-XXI/2.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    int n = 9;
+    int n = 9;  // the number of rows must be odd otherwise the output not display properly
     int a = 1;
     for (int i = 1; i <= n - 1; i++) {
       for (int j = 1; j <= n - i; j++) {
