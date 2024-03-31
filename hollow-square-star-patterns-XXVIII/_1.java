96e125f153d284b031b6ec2b29a008c18926cd3e
Prerak Pithadiya <prerakpithadiya@gmail.com>
2024-03-31 20:05:58 +0530

    Update _1.java

--- hollow-square-star-patterns-XXVIII/_1.java
+++ hollow-square-star-patterns-XXVIII/_1.java
@@ -1,8 +1,13 @@
 public class _1 {
   public static void main(String[] args) {
-    int n = 5;
+    int n = 1;
     int a = 2;
     int b = n - 1;
+
+    if (n == 1) {
+      System.out.println("1");
+      return;
+    }
 
     for (int i = 1; i <= n; i++) {
       System.out.print(i + " ");
